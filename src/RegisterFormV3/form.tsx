8496dbd74ec8bd1335e1d21cc8cf4394510132c2
Luca Noort <lucanoort@gmail.com>
2026-03-30 17:10:37 +0200

    add dropdown to fieldComponents

--- src/RegisterFormV3/form.tsx
+++ src/RegisterFormV3/form.tsx
@@ -1,12 +1,14 @@
 import { createFormHook } from "@tanstack/react-form";
 import { fieldContext, formContext } from "./form-context";
 import TextInput from "./TextInput";
+import DropDown from "./DropDown";
 
 export const {useAppForm, withForm, withFieldGroup} = createFormHook({
     fieldContext: fieldContext,
     formContext: formContext,
     fieldComponents: {
-        TextInput: TextInput
+        TextInput: TextInput,
+        DropDown: DropDown
     }, // use to pass inputs, checkboxes, selects etc. to the form, so you can use them in your form with the <form.Field> component
     formComponents: {}, // use to pass things related to the form, like a submit button, to the form, so you can use them in your form with the <form.Form> component
 })
