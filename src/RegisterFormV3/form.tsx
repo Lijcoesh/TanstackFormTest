d88a25f74c12326d9351fa4a88cbc5ebde39d22e
Luca Noort <lucanoort@gmail.com>
2026-03-30 16:44:26 +0200

    add TextInput to form

--- src/RegisterFormV3/form.tsx
+++ src/RegisterFormV3/form.tsx
@@ -1,9 +1,12 @@
 import { createFormHook } from "@tanstack/react-form";
 import { fieldContext, formContext } from "./form-context";
+import TextInput from "./TextInput";
 
 export const {useAppForm, withForm, withFieldGroup} = createFormHook({
     fieldContext: fieldContext,
     formContext: formContext,
-    fieldComponents: {}, // use to pass inputs, checkboxes, selects etc. to the form, so you can use them in your form with the <form.Field> component
+    fieldComponents: {
+        TextInput: TextInput
+    }, // use to pass inputs, checkboxes, selects etc. to the form, so you can use them in your form with the <form.Field> component
     formComponents: {}, // use to pass things related to the form, like a submit button, to the form, so you can use them in your form with the <form.Form> component
 })
