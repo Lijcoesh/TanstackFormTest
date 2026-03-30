e1ab96e0ee694fb2dc7b7d1cf489fdefa00ab902
Luca Noort <lucanoort@gmail.com>
2026-03-30 16:02:00 +0200

    add username field using tanstack form

--- src/RegisterFormV2/RegisterForm.tsx
+++ src/RegisterFormV2/RegisterForm.tsx
@@ -35,6 +35,22 @@ export default function RegisterForm() {
                     form.handleSubmit()
                 }}
             >
+                <div className="form-row">
+                    <form.Field name="username">
+                        {(field) => (
+                            <>
+                                <label htmlFor="username">Username</label>
+                                <input className="form-input" 
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    value={field.state.value}
+                                    onChange={(e) => field.handleChange(e.target.value)}
+                                />
+                            </>
+                        )}
+                    </form.Field>
+                </div>
 
                 <div className="form-row actions">
                     <button className="submit-btn" type="submit">Submit</button>
