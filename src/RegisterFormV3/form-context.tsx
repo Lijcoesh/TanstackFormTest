571e4df2f3bc67c62f829ef6e5f71a64912b5696
Luca Noort <lucanoort@gmail.com>
2026-03-30 16:36:37 +0200

    add comment to form-context to explain why we need it

--- src/RegisterFormV3/form-context.tsx
+++ src/RegisterFormV3/form-context.tsx
@@ -1,3 +1,5 @@
 import { createFormHookContexts } from "@tanstack/react-form";
 
+// createFormHookContexts is used to create a context for the form and fields, 
+// so you can use the useFormContext and useFieldContext hooks to access the form and field state and actions in your components
 export const { fieldContext, formContext, useFieldContext, useFormContext } = createFormHookContexts();
