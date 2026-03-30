2feb2155f725cb27a434561dd8d5bc946e0bb7ed
Luca Noort <lucanoort@gmail.com>
2026-03-30 17:03:34 +0200

    replace all fields with appfield and use field.TextInput

--- src/RegisterFormV3/RegisterFormV3.tsx
+++ src/RegisterFormV3/RegisterFormV3.tsx
@@ -41,7 +41,8 @@ export default function RegisterForm() {
                             );
                         }}
                     </form.AppField>
-                    <form.Field name="email"
+                    <form.AppField 
+                        name="email"
                         validators={{
                             onChange: ({value}) => {
                                 if(value.trim() === '') {
@@ -53,119 +54,90 @@ export default function RegisterForm() {
                             },
                         }}
                     >
-                        {(field) => (
-                            <>
-                                <label htmlFor="email">Email</label>
-                                <input className="form-input" 
-                                    type="email"
+                        {(field) => {
+                            return (
+                                <field.TextInput
                                     id="email"
-                                    name="email"
-                                    value={field.state.value}
-                                    onChange={(e) => field.handleChange(e.target.value)}
+                                    label="Email"
                                 />
-                                {field.state.meta.errors.length > 0 && (
-                                    <em>{field.state.meta.errors.join(', ')}</em>
-                                )}
-                            </>
-                        )}
-                    </form.Field>
-                    <form.Field name="password" validators={{
-                        onChange: ({value}) => {
-                            if(value.trim() === '') {
-                                return 'Password is required'
-                            }
-                            if(value.length < 6) {
-                                return 'Password must be at least 6 characters'
-                            }
-                        },
-                    }}>
-                        {(field) => (
-                            <>
-                                <label htmlFor="password">Password</label>
-                                <input className="form-input"
-                                    type="password"
+                            );
+                        }}
+                    </form.AppField>
+                    <form.AppField 
+                        name="password" 
+                        validators={{
+                            onChange: ({value}) => {
+                                if(value.trim() === '') {
+                                    return 'Password is required'
+                                }
+                                if(value.length < 6) {
+                                    return 'Password must be at least 6 characters'
+                                }
+                            },
+                        }}
+                    >
+                        {(field) => {
+                            return (
+                                <field.TextInput
                                     id="password"
-                                    name="password"
-                                    value={field.state.value}
-                                    onChange={(e) => field.handleChange(e.target.value)}
+                                    label="Password"
                                 />
-                                {field.state.meta.errors.length > 0 && (
-                                    <em>{field.state.meta.errors.join(', ')}</em>
-                                )}
-                            </>
-                        )}
-                    </form.Field>
-                    <form.Field name="confirmPassword" validators={{
-                        onChangeListenTo: ['password'],
-                        onChange: ({value, fieldApi}) => {
-                            if(value.trim() === '') {
-                                return 'Confirm Password is required'
-                            }
-                            if(value !== fieldApi.form.getFieldValue('password')) {
-                                return 'Passwords do not match'
-                            }
-                        },
-                    }}>
-                        {(field) => (
-                            <>
-                                <label htmlFor="confirmPassword">Confirm Password</label>
-                                <input className="form-input"
-                                    type="password"
+                            );
+                        }}
+                    </form.AppField>
+                    <form.AppField 
+                        name="confirmPassword" 
+                        validators={{
+                            onChangeListenTo: ['password'],
+                            onChange: ({value, fieldApi}) => {
+                                if(value.trim() === '') {
+                                    return 'Confirm Password is required'
+                                }
+                                if(value !== fieldApi.form.getFieldValue('password')) {
+                                    return 'Passwords do not match'
+                                }
+                            },
+                        }}
+                    >
+                        {(field) => {
+                            return (
+                                <field.TextInput
                                     id="confirmPassword"
-                                    name="confirmPassword"
-                                    value={field.state.value}
-                                    onChange={(e) => field.handleChange(e.target.value)}
+                                    label="Confirm Password"
                                 />
-                                {field.state.meta.errors.length > 0 && (
-                                    <em>{field.state.meta.errors.join(', ')}</em>
-                                )}
-                            </>
-                        )}
-                    </form.Field>
-                    <form.Field name="age" validators={{
-                        onChange: ({value}) => {
-                            if(value <= 0) {
-                                return 'Age must be greater than 0'
-                            }
-                        },
-                    }}>
-                        {(field) => (
-                            <>
-                                <label htmlFor="age">Age</label>
-                                <input className="form-input"
-                                    type="number"
+                            );
+                        }}
+                    </form.AppField>
+                    <form.AppField 
+                        name="age" 
+                        validators={{
+                            onChange: ({value}) => {
+                                if(value <= 0) {
+                                    return 'Age must be greater than 0'
+                                }
+                            },
+                        }}
+                    >
+                        {(field) => {
+                            return (
+                                <field.TextInput
                                     id="age"
-                                    name="age"
-                                    value={field.state.value}
-                                    onChange={(e) => field.handleChange(Number(e.target.value))}
+                                    label="Age"
                                 />
-                                {field.state.meta.errors.length > 0 && (
-                                    <em>{field.state.meta.errors.join(', ')}</em>
-                                )}
-                            </>
-                        )}
-                    </form.Field>
-                    <form.Field name="nationality">
-                        {(field) => (
-                            <>
-                                <label htmlFor="nationality">Nationality</label>
-                                <select className="form-input"
+                            );
+                        }}
+                    </form.AppField>
+                    <form.AppField 
+                        name="nationality">
+                        {(field) => {
+                            return (
+                                <field.TextInput
                                     id="nationality"
-                                    name="nationality"
-                                    value={field.state.value}
-                                    onChange={(e) => field.handleChange(e.target.value)}
-                                >
-                                    <option value="">Select Nationality</option>
-                                    <option value="us">United States</option>
-                                    <option value="ca">Canada</option>
-                                    <option value="uk">United Kingdom</option>
-                                </select>
-                                {field.state.meta.errors.length > 0 && (
-                                    <em>{field.state.meta.errors.join(', ')}</em>
-                                )}
-                            </>
-                        )}
-                    </form.Field>
+                                    label=" Nationality"
+                                />
+                            );
+                        }}
+                    </form.AppField>
                 </div>
 
                 <div className="form-row actions">
