4353598e5d9fddf8de70f4c76a6dd7a8c9733aac
Luca Noort <lucanoort@gmail.com>
2026-03-30 16:28:47 +0200

    use RegisterFormValues as type on defaultvalues in shared.ts

--- src/RegisterFormV3/shared.ts
+++ src/RegisterFormV3/shared.ts
@@ -1,4 +1,5 @@
 import { formOptions } from "@tanstack/react-form";
+import type { RegisterFormvalues } from "./RegisterFormValues";
 
 export const defaultValues = {
     username: '',
@@ -7,7 +8,7 @@ export const defaultValues = {
     confirmPassword: '',
     age: 0,
     nationality: 'Netherlands'
-}
+} as RegisterFormvalues
 
 export const registerFormOpts = formOptions({
     defaultValues,
