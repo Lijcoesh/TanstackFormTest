8575b435b93aec45e3bd44e0006893bf64a32b58
Luca Noort <lucanoort@gmail.com>
2026-03-31 15:55:47 +0200

    refactor dropdown to use MUI

--- src/RegisterFormInStepsV3/fieldComponents/Dropdown.tsx
+++ src/RegisterFormInStepsV3/fieldComponents/Dropdown.tsx
@@ -1,4 +1,5 @@
-import { useFieldContext } from "../form/form-context"
+import { Box, InputLabel, MenuItem, Select } from '@mui/material'
+import { useFieldContext } from '../form/form-context'
 
 interface IProps {
     label: string
@@ -11,19 +12,26 @@ export default function Dropdown(props: IProps) {
     const field = useFieldContext<string>()
 
     return (
-        <>
-            <label htmlFor={id}>{label}</label>
-            <select className="form-input"
+        <Box 
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 1,
+                }}        
+            >
+            <InputLabel id={id}>{label}</InputLabel>
+            <Select
+                labelId={id}
                 id={id}
-                name={id}
                 value={field.state.value}
+                label={label}
                 onChange={(e) => field.handleChange(e.target.value)}
             >
-                <option value="">Netherlands</option>
-                <option value="us">United States</option>
-                <option value="ca">Canada</option>
-                <option value="uk">United Kingdom</option>
-            </select>
-        </>
+                <MenuItem value="Netherlands">Netherlands</MenuItem>
+                <MenuItem value="belgium">Belgium</MenuItem>
+                <MenuItem value="france">France</MenuItem>
+                <MenuItem value="germany">Germany</MenuItem>
+            </Select>
+        </Box>
     )
 }
