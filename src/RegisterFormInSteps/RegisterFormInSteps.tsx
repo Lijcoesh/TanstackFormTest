9803632d71b1ab238e8c8864ff240b5c5f44af64
Luca Noort <lucanoort@gmail.com>
2026-03-31 13:41:46 +0200

    create RegisterFormInSteps

---
+++ src/RegisterFormInSteps/RegisterFormInSteps.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Step1 } from "./StepsComponents/Step1/Step1";
+import { Step2 } from "./StepsComponents/Step2/Step2";
+import { Step3 } from "./StepsComponents/Step3/Step3";
+import { useAppForm } from "./form/form";
+import { registerFormOpts } from "./form/shared";
+import NextStepButton from "./formComponents/NextStepButton";
+
+export default function RegisterFormInSteps() {
+    const [currentStep, setCurrentStep] = useState(0);
+    const [disableButton, setDisableButton] = useState(false)
+    
+    const form = useAppForm({
+        ...registerFormOpts,
+        onSubmit: (values) => {
+            console.log(values)
+        }
+    })
+
+    const handleNextStep = () => {
+        if(currentStep == 1) {
+            setDisableButton(true);
+        }
+        setCurrentStep((prevStep) => prevStep + 1);
+    }
+
+    return(
+        <div className="register-container">
+            <h1 className="register-title">Register</h1>
+            <p> ! TODO ! </p>
+            <form.AppForm>
+                <form className="register-form"
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        form.handleSubmit()
+                    }}
+                >
+                    <div className="form-row">
+                        {currentStep === 0 && (
+                            <Step1 form={form} />
+                        )} 
+                        {currentStep === 1 && (
+                            <Step2 form={form} />
+                        )}
+                        {currentStep === 2 && (
+                            <Step3 form={form} />
+                        )}
+                    </div>
+                </form>
+            </form.AppForm>
+            <NextStepButton onClick={handleNextStep} disabled={disableButton} />
+        </div>
+    )
+}
