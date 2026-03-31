f1ecc882e7d83ef0a6637f8c87dc40935c1679e8
Luca Noort <lucanoort@gmail.com>
2026-03-31 16:08:51 +0200

    improve styling of dropdown

--- src/RegisterFormInStepsV3/fieldComponents/Dropdown.tsx
+++ src/RegisterFormInStepsV3/fieldComponents/Dropdown.tsx
@@ -1,4 +1,4 @@
-import { Box, InputLabel, MenuItem, Select } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import { useFieldContext } from '../form/form-context'
 
 interface IProps {
@@ -12,14 +12,9 @@ export default function Dropdown(props: IProps) {
     const field = useFieldContext<string>()
 
     return (
-        <Box 
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: 1,
-                }}        
-            >
-            <InputLabel id={id}>{label}</InputLabel>
+        <FormControl sx={{ mb: 2 }} fullWidth>
+            <InputLabel 
+                id={id}>{label}</InputLabel>
             <Select
                 labelId={id}
                 id={id}
@@ -32,6 +27,6 @@ export default function Dropdown(props: IProps) {
                 <MenuItem value="france">France</MenuItem>
                 <MenuItem value="germany">Germany</MenuItem>
             </Select>
-        </Box>
+        </FormControl>
     )
 }
