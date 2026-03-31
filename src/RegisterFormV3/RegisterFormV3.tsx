1eaa0f6ea867a456d3d36cb0d98a27788b90bd2b
Luca Noort <lucanoort@gmail.com>
2026-03-31 09:37:01 +0200

    use passwordForm in registerformV3

--- src/RegisterFormV3/RegisterFormV3.tsx
+++ src/RegisterFormV3/RegisterFormV3.tsx
@@ -1,6 +1,7 @@
 import "../Styling/RegisterForm.css"
 import { registerFormOpts } from "./form/shared"
 import { useAppForm } from "./form/form"
+import { PasswordForm } from "./components/passwordForm"
 
 export default function RegisterForm() {
     const form = useAppForm({
@@ -64,53 +65,7 @@ export default function RegisterForm() {
                                 );
                             }}
                         </form.AppField>
-                        <form.AppField 
-                            name="password" 
-                            validators={{
-                                onChange: ({value}) => {
-                                    if(value.trim() === '') {
-                                        return 'Password is required'
-                                    }
-                                    if(value.length < 6) {
-                                        return 'Password must be at least 6 characters'
-                                    }
-                                },
-                            }}
-                        >
-                            {(field) => {
-                                return (
-                                    <field.TextInput
-                                        id="password"
-                                        label="Password"
-                                        type="password"
-                                    />
-                                );
-                            }}
-                        </form.AppField>
-                        <form.AppField 
-                            name="confirmPassword" 
-                            validators={{
-                                onChangeListenTo: ['password'],
-                                onChange: ({value, fieldApi}) => {
-                                    if(value.trim() === '') {
-                                        return 'Confirm Password is required'
-                                    }
-                                    if(value !== fieldApi.form.getFieldValue('password')) {
-                                        return 'Passwords do not match'
-                                    }
-                                },
-                            }}
-                        >
-                            {(field) => {
-                                return (
-                                    <field.TextInput
-                                        id="confirmPassword"
-                                        label="Confirm Password"
-                                        type="password"
-                                    />
-                                );
-                            }}
-                        </form.AppField>
+                        <PasswordForm form={form} />
                         <form.AppField 
                             name="age" 
                             validators={{
