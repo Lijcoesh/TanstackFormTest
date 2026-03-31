0a2ee7e510f65dcce71e9899c9d26a54d549b55f
Luca Noort <lucanoort@gmail.com>
2026-03-31 10:40:05 +0200

    create positive number validator

--- src/RegisterFormV4/Validators/validators.ts
+++ src/RegisterFormV4/Validators/validators.ts
@@ -35,4 +35,13 @@ export const validators = {
             }
         },
     }),
+
+    // positive number validator
+    isPositiveNumber: (fieldName: string = 'This field') => ({
+        onChange: ({ value }: { value: number }) => {
+            if (value <= 0) {
+                return `${fieldName} must be greater than 0`
+            }
+        },
+    }),
 }
