f49ccc1be1562a1afd0fcc64836829f250681502
Luca Noort <lucanoort@gmail.com>
2026-03-31 13:45:52 +0200

    add StepButton to formComponents

--- src/RegisterFormInSteps/form/form.tsx
+++ src/RegisterFormInSteps/form/form.tsx
@@ -3,7 +3,7 @@ import { fieldContext, formContext } from "./form-context";
 import TextInput from "../../RegisterFormInSteps/fieldComponents/TextInput";
 import Dropdown from "../../RegisterFormInSteps/fieldComponents/Dropdown";
 import SubmitButton from "../formComponents/SubmitButton";
-import NextStepButton from "../formComponents/NextStepButton";
+import StepButton from "../formComponents/StepButton";
 
 export const {useAppForm, withForm, withFieldGroup} = createFormHook({
     fieldContext: fieldContext,
@@ -14,6 +14,6 @@ export const {useAppForm, withForm, withFieldGroup} = createFormHook({
     }, // use to pass inputs, checkboxes, selects etc. to the form, so you can use them in your form with the <form.Field> component
     formComponents: {
         SubmitButton,
-        NextStepButton,
+        StepButton,
     }, // use to pass things related to the form, like a submit button, to the form, so you can use them in your form with the <form.Form> component
 })
