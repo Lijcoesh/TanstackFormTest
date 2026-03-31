02de0fbbef675b8742ca4007f14d55f91180c74a
Luca Noort <lucanoort@gmail.com>
2026-03-31 15:32:47 +0200

    fix errors in RegisterFOrmInStepsSteps

--- src/RegisterFormInStepsV2/RegisterFormInStepsSteps.tsx
+++ src/RegisterFormInStepsV2/RegisterFormInStepsSteps.tsx
@@ -1,4 +1,4 @@
-import { withForm } from "./form/form"
+import { useAppForm } from "./form/form"
 import { registerFormOpts } from "./form/shared"
 import { Step1 } from "./StepsComponents/Step1/Step1"
 import { Step2 } from "./StepsComponents/Step2/Step2"
@@ -9,31 +9,35 @@ interface IProps {
     showSubmitButton: boolean
 }
 
-export const RegisterFormInStepsSteps = withForm({
-    ...registerFormOpts,
-    render: ({ form }) => {
-        return (
-            <form.AppForm>
-                <form className="register-form"
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                        form.handleSubmit()
-                    }}
-                >
-                    <div className="form-row">
-                        {currentStep === 0 && (
-                            <Step1 form={form} />
-                        )} 
-                        {currentStep === 1 && (
-                            <Step2 form={form} />
-                        )}
-                        {currentStep === 2 && (
-                            <Step3 form={form} />
-                        )}
-                    </div>
-                    <form.SubmitButton />
-                </form>
-            </form.AppForm>
-        )
-    }
-})
+export function RegisterFormInStepsSteps({ currentStep, showSubmitButton }: IProps) {
+    const form = useAppForm({
+        ...registerFormOpts,
+        onSubmit: (values) => {
+            console.log(values, "form submitted")
+        }
+    })
+
+    return (
+        <form.AppForm>
+            <form className="register-form"
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    form.handleSubmit()
+                }}
+            >
+                <div className="form-row">
+                    {currentStep === 0 && (
+                        <Step1 form={form} />
+                    )} 
+                    {currentStep === 1 && (
+                        <Step2 form={form} />
+                    )}
+                    {currentStep === 2 && (
+                        <Step3 form={form} />
+                    )}
+                </div>
+                {showSubmitButton && <form.SubmitButton />}
+            </form>
+        </form.AppForm>
+    )
+}
