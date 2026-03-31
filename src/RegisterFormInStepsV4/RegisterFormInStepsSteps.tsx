8869e586093399863301ab264bccfc6e708dd815
Luca Noort <lucanoort@gmail.com>
2026-03-31 17:08:42 +0200

    add console.log popualtion coint per nationality

--- src/RegisterFormInStepsV4/RegisterFormInStepsSteps.tsx
+++ src/RegisterFormInStepsV4/RegisterFormInStepsSteps.tsx
@@ -14,6 +14,7 @@ export function RegisterFormInStepsSteps({ currentStep }: IProps) {
         ...registerFormOpts,
         onSubmit: (values) => {
             console.log(values, "form submitted")
+            console.log(values.value.nationality.populationCount, "population count")
         }
     })
 
