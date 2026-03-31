21d9065fb1376ca6377482bad379c7cdf8c3e515
Luca Noort <lucanoort@gmail.com>
2026-03-31 11:10:39 +0200

    add more validation to usernameform

--- src/RegisterFormV4/UserInformation/components/UsernameForm.tsx
+++ src/RegisterFormV4/UserInformation/components/UsernameForm.tsx
@@ -1,5 +1,6 @@
 import { withForm } from "../../form/form"
 import { registerFormOpts } from "../../form/shared"
+import { StringValidators } from "../../Validators/StringValidators"
 import { validators } from "../../Validators/validators"
 
 {/* use form.AppField because it provides the context needed for the text input component */}
@@ -9,7 +10,11 @@ export const UsernameForm = withForm({
         return (
             <form.AppField
                 name="username" 
-                validators={validators.isRequired('Username')}
+                validators={{
+                    ...validators.isRequired('Username'),
+                    ...StringValidators.minLength(3, 'Username'),
+                    ...StringValidators.maxLength(20, 'Username'),
+                }}
             >
                 {(field) => {
                     return (
